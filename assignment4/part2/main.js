e3a5ddddfc8235a2ac29412b2ae99e4a40bed147
sapkotaashok00 <ashokraj.sapkota@dcmail.ca>
2024-08-02 13:05:13 -0400

    updated

--- assignment4/part2/main.js
+++ assignment4/part2/main.js
@@ -6,14 +6,15 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 const images = ['pic1.jpg', `pic2.jpg`, `pic3.jpg`, `pic4.jpg`, `pic5.jpg`];
-const alts
-/* Declaring the alternative text for each image file */
+const alts = {
+    'pic1.jpg': 'Closeup of a human eye',
+    /* Declaring the alternative text for each image file */
 
-/* Looping through images */
+    /* Looping through images */
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', xxx);
+    newImage.setAttribute('alt', xxx);
+    thumbBar.appendChild(newImage);
 
 /* Wiring up the Darken/Lighten button */
