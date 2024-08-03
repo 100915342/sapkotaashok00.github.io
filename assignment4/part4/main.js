e5a8bdcd0db05865f86716c46f99aaf06b7b5939
sapkotaashok00 <ashokraj.sapkota@dcmail.ca>
2024-08-02 22:29:49 -0400

    Code changed

--- assignment4/part4/main.js
+++ assignment4/part4/main.js
@@ -22,57 +22,56 @@ function randomRGB() {
   return rgb(${ random(0, 255) }, ${ random(0, 255) }, ${ random(0, 255) });
 }
 
-class Ball {
-  constructor(x, y, velX, velY, color, size) {
+class Shape {
+  constructor(x, y, velX, velY) {
     this.x = x;
     this.y = y;
     this.velX = velX;
     this.velY = velY;
-    this.color = color;
-    this.size = size;
+  }
+}
+
+draw() {
+  ctx.beginPath();
+  ctx.fillStyle = this.color;
+  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+  ctx.fill();
+}
+
+update() {
+  if (this.x + this.size >= width) {
+    this.velX = -Math.abs(this.velX);
   }
 
-  draw() {
-    ctx.beginPath();
-    ctx.fillStyle = this.color;
-    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-    ctx.fill();
+  if (this.x - this.size <= 0) {
+    this.velX = Math.abs(this.velX);
   }
 
-  update() {
-    if (this.x + this.size >= width) {
-      this.velX = -Math.abs(this.velX);
-    }
-
-    if (this.x - this.size <= 0) {
-      this.velX = Math.abs(this.velX);
-    }
-
-    if (this.y + this.size >= height) {
-      this.velY = -Math.abs(this.velY);
-    }
-
-    if (this.y - this.size <= 0) {
-      this.velY = Math.abs(this.velY);
-    }
-
-    this.x += this.velX;
-    this.y += this.velY;
+  if (this.y + this.size >= height) {
+    this.velY = -Math.abs(this.velY);
   }
 
-  collisionDetect() {
-    for (const ball of balls) {
-      if (!(this === ball)) {
-        const dx = this.x - ball.x;
-        const dy = this.y - ball.y;
-        const distance = Math.sqrt(dx * dx + dy * dy);
+  if (this.y - this.size <= 0) {
+    this.velY = Math.abs(this.velY);
+  }
 
-        if (distance < this.size + ball.size) {
-          ball.color = this.color = randomRGB();
-        }
+  this.x += this.velX;
+  this.y += this.velY;
+}
+
+collisionDetect() {
+  for (const ball of balls) {
+    if (!(this === ball)) {
+      const dx = this.x - ball.x;
+      const dy = this.y - ball.y;
+      const distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance < this.size + ball.size) {
+        ball.color = this.color = randomRGB();
       }
     }
   }
+}
 }
 
 const balls = [];
